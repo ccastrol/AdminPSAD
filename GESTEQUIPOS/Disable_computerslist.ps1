207e681a62c306835e404bbf653cdb683a0904f6
ccastrol <carloscastroleon@gmail.com>
2024-11-12 23:11:33 -0500

    arreglos

--- GESTEQUIPOS/Disable_computerslist.ps1
+++ GESTEQUIPOS/Disable_computerslist.ps1
@@ -1,11 +1,11 @@
-﻿$compu=Get-Content C:\Users\Administrador\SCRIPTS\pc_desactivar.txt
+﻿$compu=Get-Content .\pc_desactivar.txt
   
 ForEach ($compu in $compu)  
   
 {  
   
 Set-ADComputer -Identity $compu -Enabled $false
-Get-ADComputer -Identity "$compu" | Move-ADObject -TargetPath “OU=Disabled-Computers,DC=chimuasa,DC=com”
+Get-ADComputer -Identity "$compu" | Move-ADObject -TargetPath “OU=Disabled-Computers,DC=ccastrol,DC=com”
   
 write-host "compu $($compu) has been disabled"  
   
